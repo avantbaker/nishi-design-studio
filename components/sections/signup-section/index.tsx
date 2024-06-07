f624603dd1d382c03923fa346469c6ec38dbfbb6
Avant Baker <b.avant89@gmail.com>
2024-06-07 12:00:17 -0500

    Added the new video and removed some newsletter stuff

--- components/sections/signup-section/index.tsx
+++ components/sections/signup-section/index.tsx
@@ -1,134 +1,134 @@
-import styled from 'styled-components';
-import theme from 'styles/theme';
-import Image from 'next/image';
-import { Flex } from 'rebass/styled-components';
-import { rem } from 'polished';
-import Text from 'components/common/text';
-import Input from 'components/common/input';
-import { PrimaryButton } from 'components/common/button';
-import { breakpoints, queries } from 'styles/media';
-import useMediaQuery from 'hooks/use-media-query';
-import { useForm } from '@formspree/react';
-import { useState, useEffect, useMemo } from 'react';
+import styled from "styled-components";
+import theme from "styles/theme";
+import Image from "next/image";
+import { Flex } from "rebass/styled-components";
+import { rem } from "polished";
+import Text from "components/common/text";
+import Input from "components/common/input";
+import { PrimaryButton } from "components/common/button";
+import { breakpoints, queries } from "styles/media";
+import useMediaQuery from "hooks/use-media-query";
+import { useForm } from "@formspree/react";
+import { useState, useEffect, useMemo } from "react";
 
 const Container = styled.div`
-	background: ${theme.colors.brown};
-	position: relative;
-	padding: ${rem(43)} ${rem(28)} ${rem(118)} ${rem(28)};
+  background: ${theme.colors.brown};
+  position: relative;
+  padding: ${rem(43)} ${rem(28)} ${rem(118)} ${rem(28)};
 
-	.center-image {
-		z-index: 1;
-	}
+  .center-image {
+    z-index: 1;
+  }
 
-	.nishi-logo {
-		position: absolute;
-		left: 50%;
-		transform: translate(-50%, 40%);
-	}
+  .nishi-logo {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, 40%);
+  }
 
-	.nishi-logo-mobile {
-		position: absolute;
-		left: 50%;
-		transform: translate(-50%, 42%);
-	}
+  .nishi-logo-mobile {
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, 42%);
+  }
 
-	@media only screen and (min-width: ${breakpoints.tablet}) {
-		padding-top: ${rem(101)};
-		padding-bottom: ${rem(203)};
-	}
+  @media only screen and (min-width: ${breakpoints.tablet}) {
+    padding-top: ${rem(101)};
+    padding-bottom: ${rem(203)};
+  }
 `;
 
 const Form = styled.form`
-	width: 100%;
-	margin-bottom: ${rem(50)};
-	display: flex;
-	flex-direction: column;
-	flex-grow: 1;
-	justify-content: flex-end;
+  width: 100%;
+  margin-bottom: ${rem(50)};
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: flex-end;
 
-	input {
-		margin-bottom: ${rem(14)};
-	}
+  input {
+    margin-bottom: ${rem(14)};
+  }
 
-	@media only screen and (min-width: ${breakpoints.tablet}) {
-		width: initial;
-		flex-direction: row;
-		margin-bottom: 0;
+  @media only screen and (min-width: ${breakpoints.tablet}) {
+    width: initial;
+    flex-direction: row;
+    margin-bottom: 0;
 
-		${Input} {
-			margin-bottom: 0;
-			max-width: ${rem(299)};
-			margin-right: ${rem(15)};
-			flex-grow: 1;
-		}
+    ${Input} {
+      margin-bottom: 0;
+      max-width: ${rem(299)};
+      margin-right: ${rem(15)};
+      flex-grow: 1;
+    }
 
-		${PrimaryButton} {
-			width: ${rem(172)};
-		}
-	}
+    ${PrimaryButton} {
+      width: ${rem(172)};
+    }
+  }
 `;
 
 const GoldLineLeft = styled.img`
-	position: absolute;
-	z-index: 0;
-	left: 0;
-	bottom: ${rem(153)};
-	height: ${rem(37.62)};
-	width: ${rem(182.73)};
-	transform: scaleX(-1) rotate(-9deg);
+  position: absolute;
+  z-index: 0;
+  left: 0;
+  bottom: ${rem(153)};
+  height: ${rem(37.62)};
+  width: ${rem(182.73)};
+  transform: scaleX(-1) rotate(-9deg);
 
-	@media only screen and (min-width: ${breakpoints.tablet}) {
-		transform: scaleY(1) scaleX(-1);
-		width: 35%;
-		height: ${rem(75.78)};
-		left: -${rem(5)};
-		bottom: ${rem(275)};
-	}
+  @media only screen and (min-width: ${breakpoints.tablet}) {
+    transform: scaleY(1) scaleX(-1);
+    width: 35%;
+    height: ${rem(75.78)};
+    left: -${rem(5)};
+    bottom: ${rem(275)};
+  }
 `;
 const GoldLineRight = styled.img`
-	width: 33%;
-	top: ${rem(350)};
-	position: absolute;
-	right: -${rem(95)};
+  width: 33%;
+  top: ${rem(350)};
+  position: absolute;
+  right: -${rem(95)};
 
-	@media only screen and (min-width: ${breakpoints.tablet}) {
-		right: 0;
-		top: ${rem(300)};
-	}
+  @media only screen and (min-width: ${breakpoints.tablet}) {
+    right: 0;
+    top: ${rem(300)};
+  }
 `;
 
 const myLoader = ({ src, width, quality }) => {
-	return `${src}?w=${width}&q=100`;
+  return `${src}?w=${width}&q=100`;
 };
 
 export default function SignupSection({
-	newsletterTitle: title,
-	newsletterImage: image,
+  newsletterTitle: title,
+  newsletterImage: image,
 }) {
-	const isTablet = useMediaQuery(queries.minTablet);
-	const [state, handleSubmit] = useForm('mzbojlrn');
-	const [showSuccess, setShowSuccess] = useState(false);
-	useEffect(() => {
-		if (state.succeeded) {
-			setShowSuccess(true);
-			setTimeout(() => {
-				setShowSuccess(false);
-			}, 3000);
-		}
-	}, [state]);
+  const isTablet = useMediaQuery(queries.minTablet);
+  const [state, handleSubmit] = useForm("mzbojlrn");
+  const [showSuccess, setShowSuccess] = useState(false);
+  useEffect(() => {
+    if (state.succeeded) {
+      setShowSuccess(true);
+      setTimeout(() => {
+        setShowSuccess(false);
+      }, 3000);
+    }
+  }, [state]);
 
-	const newsletterImage = useMemo(() => {
-		const hasImage = image && image?.url;
-		const qs = '?w=1770&h=1770&q=90&fit=clip';
-		const currentImage = hasImage && `${hasImage}${qs}`;
-		const DETAULT_IMG = `https://nishidesignstudio.imgix.net/something-socal/12.jpg${qs}`;
-		return currentImage || DETAULT_IMG;
-	}, [image]);
+  const newsletterImage = useMemo(() => {
+    const hasImage = image && image?.url;
+    const qs = "?w=1770&h=1770&q=90&fit=clip";
+    const currentImage = hasImage && `${hasImage}${qs}`;
+    const DETAULT_IMG = `https://nishidesignstudio.imgix.net/something-socal/12.jpg${qs}`;
+    return currentImage || DETAULT_IMG;
+  }, [image]);
 
-	return (
-		<Container>
-			<Flex flexDirection="column" maxWidth="890px" m="0 auto">
-				<Flex
+  return (
+    <Container>
+      <Flex flexDirection="column" maxWidth="890px" m="0 auto">
+        {/* <Flex
 					flexDirection={['column', 'column', 'row']}
 					justifyContent={[null, 'space-between', 'space-between']}
 					alignItems="center"
@@ -155,53 +155,53 @@ export default function SignupSection({
 							</Text>
 						)}
 					</Form>
-				</Flex>
-				<Flex justifyContent="center" px={[rem(20), rem(20), 0]}>
-					{isTablet ? (
-						<Image
-							quality="100"
-							className="center-image"
-							alt="Join the newsletter"
-							src={newsletterImage}
-							layout="fixed"
-							width="890px"
-							height="466px"
-						/>
-					) : (
-						<Image
-							quality="100"
-							className="center-image"
-							alt="Join the newsletter"
-							src={newsletterImage}
-							width="446px"
-							height="424px"
-						/>
-					)}
-					<GoldLineLeft src="/elements/goldlines/Gold-Line-6.png" />
-					<GoldLineRight src="/images/gold-line-four.png" />
-				</Flex>
-			</Flex>
-			{isTablet ? (
-				<div className="nishi-logo">
-					<Image
-						quality="100"
-						alt="Nishi"
-						width="138px"
-						height="178px"
-						src="/images/nishi-gold.png"
-					/>
-				</div>
-			) : (
-				<div className="nishi-logo-mobile">
-					<Image
-						quality="100"
-						alt="Nishi"
-						width="76px"
-						height="98px"
-						src="/images/nishi-gold-mobile.png"
-					/>
-				</div>
-			)}
-		</Container>
-	);
+				</Flex> */}
+        <Flex justifyContent="center" px={[rem(20), rem(20), 0]}>
+          {isTablet ? (
+            <Image
+              quality="100"
+              className="center-image"
+              alt="Join the newsletter"
+              src={newsletterImage}
+              layout="fixed"
+              width="890px"
+              height="466px"
+            />
+          ) : (
+            <Image
+              quality="100"
+              className="center-image"
+              alt="Join the newsletter"
+              src={newsletterImage}
+              width="446px"
+              height="424px"
+            />
+          )}
+          <GoldLineLeft src="/elements/goldlines/Gold-Line-6.png" />
+          <GoldLineRight src="/images/gold-line-four.png" />
+        </Flex>
+      </Flex>
+      {isTablet ? (
+        <div className="nishi-logo">
+          <Image
+            quality="100"
+            alt="Nishi"
+            width="138px"
+            height="178px"
+            src="/images/nishi-gold.png"
+          />
+        </div>
+      ) : (
+        <div className="nishi-logo-mobile">
+          <Image
+            quality="100"
+            alt="Nishi"
+            width="76px"
+            height="98px"
+            src="/images/nishi-gold-mobile.png"
+          />
+        </div>
+      )}
+    </Container>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,133 +1,177 @@
-import ExpertiseCards from 'components/common/expertise-cards';
-import Footer from 'components/common/footer';
-import Nav from 'components/common/nav';
-import MediaSection from 'components/sections/media-section';
-import SignupSection from 'components/sections/signup-section';
-import SliderSection from 'components/sections/slider-section';
-import SocialSection from 'components/sections/social-section';
-import StartYourSpace from 'components/sections/start-your-space';
-import YourSpace from 'components/sections/your-space';
-import { motion } from 'framer-motion';
-import { framerOptions } from 'lib/framer';
-import { HomepageQuery } from 'lib/urql/queries/pages';
-import { getPageData } from 'lib/utils';
-import { initUrqlClient, withUrqlClient } from 'next-urql';
-import { rem } from 'polished';
-import styled from 'styled-components';
-import { breakpoints } from 'styles/media';
-import theme from 'styles/theme';
-import { cacheExchange, dedupExchange, fetchExchange, ssrExchange, useQuery } from 'urql';
-import ComingSoon from './comingsoon';
+import ExpertiseCards from "components/common/expertise-cards";
+import Footer from "components/common/footer";
+import Nav from "components/common/nav";
+import MediaSection from "components/sections/media-section";
+import SignupSection from "components/sections/signup-section";
+import SliderSection from "components/sections/slider-section";
+import SocialSection from "components/sections/social-section";
+import StartYourSpace from "components/sections/start-your-space";
+import YourSpace from "components/sections/your-space";
+import { motion } from "framer-motion";
+import { framerOptions } from "lib/framer";
+import { HomepageQuery } from "lib/urql/queries/pages";
+import { getPageData } from "lib/utils";
+import { initUrqlClient, withUrqlClient } from "next-urql";
+import { margin, rem } from "polished";
+import ReactPlayer from "react-player";
+import styled from "styled-components";
+import { breakpoints } from "styles/media";
+import theme from "styles/theme";
+import Text from "components/common/text";
+
+import {
+  cacheExchange,
+  dedupExchange,
+  fetchExchange,
+  ssrExchange,
+  useQuery,
+} from "urql";
 
 export const TopSection = styled.section`
-	position: relative;
-	overflow: hidden;
-	:before {
-		content: '';
-		background-image: url('/images/tan-bg.png');
-		background-size: cover;
-		position: absolute;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		z-index: -1;
-	}
+  position: relative;
+  overflow: hidden;
+  :before {
+    content: "";
+    background-image: url("/images/tan-bg.png");
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+  }
 
-	@media only screen and (min-width: ${breakpoints.tablet}) {
-		:before {
-			background-size: cover;
-		}
-	}
+  @media only screen and (min-width: ${breakpoints.tablet}) {
+    :before {
+      background-size: cover;
+    }
+  }
 `;
 
 const BottomLayout = styled.div`
-	position: relative;
-	overflow: hidden;
-	:before {
-		content: '';
-		background-image: url('/images/tan-bg.png');
-		background-size: cover;
-		position: absolute;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		z-index: -1;
-	}
+  position: relative;
+  overflow: hidden;
+  :before {
+    content: "";
+    background-image: url("/images/tan-bg.png");
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+  }
 
-	:after {
-		content: '';
-		background-color: ${theme.colors.lightTan};
-		position: absolute;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		height: ${rem(216)};
-		z-index: -1;
-	}
+  :after {
+    content: "";
+    background-color: ${theme.colors.lightTan};
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    height: ${rem(216)};
+    z-index: -1;
+  }
 `;
 
 function Home() {
-	const [result] = useQuery({
-		query: HomepageQuery,
-	});
+  const [result] = useQuery({
+    query: HomepageQuery,
+  });
 
-	const {
-		heroSlider,
-		twoColumnTextSection,
-		startYourSpace,
-		socialSection,
-		newsletterSection,
-		expertiseSection,
-		testimonialsSection,
-	} = getPageData(result) || {};
+  const {
+    heroSlider,
+    twoColumnTextSection,
+    startYourSpace,
+    socialSection,
+    newsletterSection,
+    expertiseSection,
+    testimonialsSection,
+  } = getPageData(result) || {};
 
-	const slides = heroSlider?.featuredPosts;
-	const showSliderSection = slides && slides.length > 0;
+  const slides = heroSlider?.featuredPosts;
+  const showSliderSection = slides && slides.length > 0;
 
-	return (
-		<motion.div {...framerOptions}>
-			<TopSection>
-				<Nav />
-				{showSliderSection && <SliderSection {...heroSlider} isFeatured />}
-			</TopSection>
-			<YourSpace {...twoColumnTextSection} />
-			<ExpertiseCards {...expertiseSection} />
-			<MediaSection {...testimonialsSection} />
-			<BottomLayout>
-				<StartYourSpace {...startYourSpace} />
-				<SocialSection {...socialSection} />
-			</BottomLayout>
-			{newsletterSection && <SignupSection {...newsletterSection} />}
-			<Footer />
-		</motion.div>
-	);
+  return (
+    <motion.div {...framerOptions}>
+      <TopSection>
+        <Nav />
+        {showSliderSection && <SliderSection {...heroSlider} isFeatured />}
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: "7rem",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              textAlign: "center",
+            }}
+          >
+            <Text
+              mb={[rem(16), rem(29)]}
+              width={[rem(250), rem(250), "auto"]}
+              variant={["headingMobile", "headingMobile", "heading"]}
+            >
+              NDS Showhouse 2024
+            </Text>
+            <Text
+              maxWidth={rem(554)}
+              variant={["body", "body", "bodyLarge"]}
+              style={{ margin: "auto", marginBottom: rem(30) }}
+            >
+              If you’re looking for something decisively different, you’ve come
+              to the right place.
+            </Text>
+          </div>
+          <ReactPlayer url="https://youtu.be/n87F5Qgcw4A" />
+        </div>
+      </TopSection>
+      <section style={{ position: "relative" }}></section>
+      <YourSpace {...twoColumnTextSection} />
+      <ExpertiseCards {...expertiseSection} />
+      <MediaSection {...testimonialsSection} />
+      <BottomLayout>
+        <StartYourSpace {...startYourSpace} />
+        <SocialSection {...socialSection} />
+      </BottomLayout>
+      {newsletterSection && <SignupSection {...newsletterSection} />}
+      <Footer />
+    </motion.div>
+  );
 }
 
 export async function getStaticProps() {
-	const ssrCache = ssrExchange({ isClient: false });
-	const client = initUrqlClient(
-		{
-			url: 'https://live-nishi-design-studio.pantheonsite.io/graphql',
-			exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
-		},
-		true
-	);
+  const ssrCache = ssrExchange({ isClient: false });
+  const client = initUrqlClient(
+    {
+      url: "https://live-nishi-design-studio.pantheonsite.io/graphql",
+      exchanges: [dedupExchange, cacheExchange, ssrCache, fetchExchange],
+    },
+    true
+  );
 
-	await client.query(HomepageQuery).toPromise();
+  await client.query(HomepageQuery).toPromise();
 
-	return {
-		props: {
-			urqlState: ssrCache.extractData(),
-		},
-		revalidate: 10,
-	};
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+    revalidate: 10,
+  };
 }
 
 export default withUrqlClient(
-	(_) => ({
-		url: 'https://live-nishi-design-studio.pantheonsite.io/graphql',
-	}),
-	{ ssr: false, staleWhileRevalidate: true } // Important so we don't wrap our component in getInitialProps
+  (_) => ({
+    url: "https://live-nishi-design-studio.pantheonsite.io/graphql",
+  }),
+  { ssr: false, staleWhileRevalidate: true } // Important so we don't wrap our component in getInitialProps
 )(Home);
